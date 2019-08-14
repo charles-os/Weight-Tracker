90e2d3d8007691cbf280e66ef2f48d5e993ff6eb
moringa <charonkobarix@yahoo.com>
2019-08-14 14:46:26 +0300

    commented the flask-login and flasksqlalchemy imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_login import LoginManager
 
 
 
