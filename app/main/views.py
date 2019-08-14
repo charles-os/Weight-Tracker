e1c85dd9282d6048fe1785c7092cceb694b315e9
moringa <charonkobarix@yahoo.com>
2019-08-14 09:15:27 +0300

    added a from ..models import

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
-
+from ..models import Reviews, User
 # Views
 @main.route('/')
 def index():
@@ -12,3 +12,12 @@ def index():
     title = 'Home'
 
     return render_template('index.html', title = title )
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
