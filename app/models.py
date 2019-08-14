a833a3f9d53cc5463ea9342e312c722f044950f3
moringa <charonkobarix@yahoo.com>
2019-08-14 08:55:08 +0300

    added a class users

--- app/models.py
+++ app/models.py
@@ -1,1 +1,11 @@
 # File for models/classes
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
